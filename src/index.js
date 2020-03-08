6753ee7c6cb05c899c1d9d5ed0fc933e632cbf48
Bembinov <enfin@inbox.ru>
2020-03-08 21:42:40 +0300

    feat

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-
 exports.min = function min (array) {
-  return 0;
+	if (array == undefined || array[0] == undefined) {
+		return 0;
+	}
+	array = array.sort(function(a, b) {
+	  return a - b;
+	});
+  	return array[0];
 }
-
 exports.max = function max (array) {
-  return 0;
+	if (array == undefined || array[0] == undefined) {
+		return 0;
+	}
+	array = array.sort(function(a, b) {
+	  return a - b;
+	});
+  	return array[array.length - 1];
 }
-
 exports.avg = function avg (array) {
-  return 0;
+	if (array == undefined || array[0] == undefined) {
+		return 0;
+	}
+	array = array.sort(function(a, b) {
+	  return a - b;
+	});	
+	let sum = 0;
+	for (var i = 0; i < array.length; i++) {
+		sum += array[i];
+	}
+  return (sum / array.length);
 }
